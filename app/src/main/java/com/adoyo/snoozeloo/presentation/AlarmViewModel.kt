e5ddfe1b74f3d56cb7daed8565578ba57b3d5486
AdoyoClifford <adoyoclifford@gmail.com>
2024-11-26 09:45:49 +0300

    feat: add alarm module for dependency injection
    Added a new module for dependency injection that provides instances of the alarm database, DAO, repository, and view model. This allows for easier management and testing of dependencies.

---
+++ app/src/main/java/com/adoyo/snoozeloo/presentation/AlarmViewModel.kt
@@ -0,0 +1,11 @@
+package com.adoyo.snoozeloo.presentation
+
+import androidx.lifecycle.ViewModel
+import com.adoyo.snoozeloo.domain.respository.AlarmRepository
+
+
+class AlarmViewModel(
+    //private val alarmRepository: AlarmRepository
+): ViewModel() {
+
+}
