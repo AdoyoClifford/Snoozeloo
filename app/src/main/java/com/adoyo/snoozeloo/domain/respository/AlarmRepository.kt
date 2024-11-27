f39f0d7e417796c84b859442dba6b00f8d10e6c3
AdoyoClifford <adoyoclifford@gmail.com>
2024-11-27 08:10:38 +0300

    Refactor: Updated AlarmRepository to use Flow and Result
    Updated the AlarmRepository interface to use Kotlin Flow and Result for better error handling and asynchronous data streams.
    
    - `getAlarms()` now returns a Flow<Result<List<Alarm>, Error>>.
    - Other methods (`getAlarm`, `addAlarm`, `removeAlarm`, `updateAlarm`) are now suspend functions and return Result<Unit, Error> or Result<Alarm, Error>.

--- app/src/main/java/com/adoyo/snoozeloo/domain/respository/AlarmRepository.kt
+++ app/src/main/java/com/adoyo/snoozeloo/domain/respository/AlarmRepository.kt
@@ -1,11 +1,14 @@
 package com.adoyo.snoozeloo.domain.respository
 
 import com.adoyo.snoozeloo.domain.model.Alarm
+import kotlinx.coroutines.flow.Flow
+import com.adoyo.snoozeloo.domain.Result
+import com.adoyo.snoozeloo.domain.Error
 
 interface AlarmRepository {
-    fun getAlarms(): List<Alarm>
-    fun getAlarm(id: Int): Alarm
-    fun addAlarm(alarm: Alarm)
-    fun removeAlarm(id: Int)
-    fun updateAlarm(alarm: Alarm)
+    fun getAlarms(): Flow<Result<List<Alarm>,Error>>
+    suspend fun getAlarm(id: Int): Result<Alarm, Error>
+    suspend fun addAlarm(alarm: Alarm): Result<Unit, Error>
+    suspend fun removeAlarm(id: Int): Result<Unit, Error>
+    suspend fun updateAlarm(alarm: Alarm): Result<Unit, Error>
 }
