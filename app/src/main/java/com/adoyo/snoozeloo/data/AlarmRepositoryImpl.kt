1949ddcbb21177c692fe1f53da84cf595f062b29
AdoyoClifford <adoyoclifford@gmail.com>
2024-11-27 08:10:48 +0300

    refactor: updated repository functions to return Result
    Updated repository functions to return Result instead of plain data. This provides a way to handle errors and success cases more explicitly, improving the application's reliability and error handling.

--- app/src/main/java/com/adoyo/snoozeloo/data/AlarmRepositoryImpl.kt
+++ app/src/main/java/com/adoyo/snoozeloo/data/AlarmRepositoryImpl.kt
@@ -1,30 +1,67 @@
 package com.adoyo.snoozeloo.data
 
 import com.adoyo.snoozeloo.data.database.AlarmDao
+import com.adoyo.snoozeloo.domain.DataError
+import com.adoyo.snoozeloo.domain.Error
+import com.adoyo.snoozeloo.domain.Result
+import com.adoyo.snoozeloo.domain.Result.Success
 import com.adoyo.snoozeloo.domain.model.Alarm
 import com.adoyo.snoozeloo.domain.respository.AlarmRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 
 class AlarmRepositoryImpl(
     private val alarmDao: AlarmDao
 ) : AlarmRepository {
-
-    override fun getAlarms(): List<Alarm> {
-        return  alarmDao.getAlarms()
+    override fun getAlarms(): Flow<Result<List<Alarm>, Error>> = flow {
+        try {
+            alarmDao.getAlarms()
+                .collect { alarms ->
+                    emit(Success(alarms))
+                }
+        } catch (e: Exception) {
+            emit(Result.Error(DataError.Local.DATABASE_ERROR))
+        }
     }
 
-    override fun getAlarm(id: Int): Alarm {
-        return alarmDao.getAlarm(id)
+    override suspend fun getAlarm(id: Int): Result<Alarm, Error> {
+        return try {
+            val alarm = alarmDao.getAlarm(id)
+            if (true) {
+                return Success(alarm)
+            } else {
+                Result.Error(DataError.Local.FILE_NOT_FOUND)
+            }
+
+        } catch (e: Exception) {
+            Result.Error(DataError.Local.DATABASE_ERROR)
+        }
     }
 
-    override fun addAlarm(alarm: Alarm) {
-        alarmDao.upsertAlarm(alarm)
+    override suspend fun addAlarm(alarm: Alarm): Result<Unit, Error> {
+        return try {
+            alarmDao.upsertAlarm(alarm)
+            Success(Unit)
+        } catch (e: Exception) {
+            Result.Error(DataError.Local.DATABASE_ERROR)
+        }
     }
 
-    override fun removeAlarm(id: Int) {
-        alarmDao.removeAlarm(id)
+    override suspend fun removeAlarm(id: Int): Result<Unit, Error> {
+        return try {
+            alarmDao.removeAlarm(id)
+            Success(Unit)
+        } catch (e: Exception) {
+            Result.Error(DataError.Local.DATABASE_ERROR)
+        }
     }
 
-    override fun updateAlarm(alarm: Alarm) {
-        alarmDao.upsertAlarm(alarm)
+    override suspend fun updateAlarm(alarm: Alarm): Result<Unit, Error> {
+        return try {
+            alarmDao.upsertAlarm(alarm)
+            Success(Unit)
+        } catch (e: Exception) {
+            Result.Error(DataError.Local.DATABASE_ERROR)
+        }
     }
 }
