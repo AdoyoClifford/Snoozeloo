fd267d7275408c76a4a73b0971d0109ffe878ae6
AdoyoClifford <adoyoclifford@gmail.com>
2024-11-27 08:11:03 +0300

    feat: implemented AlarmViewModel logic
    Implemented the AlarmViewModel to handle alarm-related operations:
    
    - Fetches and exposes the list of alarms using a StateFlow.
    - Provides functions to add, remove, and update alarms in the repository.
    - Allows selecting and clearing a selected alarm for detailed view.
    - Uses coroutines and viewModelScope for asynchronous operations.

--- app/src/main/java/com/adoyo/snoozeloo/presentation/AlarmViewModel.kt
+++ app/src/main/java/com/adoyo/snoozeloo/presentation/AlarmViewModel.kt
@@ -1,11 +1,73 @@
 package com.adoyo.snoozeloo.presentation
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.adoyo.snoozeloo.domain.model.Alarm
+import com.adoyo.snoozeloo.domain.onSuccess
 import com.adoyo.snoozeloo.domain.respository.AlarmRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 
 
 class AlarmViewModel(
-    //private val alarmRepository: AlarmRepository
+    private val alarmRepository: AlarmRepository
 ): ViewModel() {
+
+    private val _state = MutableStateFlow(AlarmState())
+    val state = _state.asStateFlow()
+
+    init {
+        getAlarms()
+    }
+
+    fun getAlarms() {
+        viewModelScope.launch{
+            alarmRepository.getAlarms().collect { result ->
+                result.onSuccess {alarms ->
+                    _state.value = state.value.copy(alarms = alarms)
+                }
+            }
+        }
+    }
+
+    fun addAlarm(alarm: Alarm) {
+        viewModelScope.launch {
+            alarmRepository.addAlarm(alarm).onSuccess{
+                getAlarms()
+            }
+        }
+    }
+
+    fun removeAlarm(id: Int) {
+        viewModelScope.launch {
+            alarmRepository.removeAlarm(id).onSuccess {
+                getAlarms()
+            }
+        }
+    }
+
+    fun upDateAlarm(alarm: Alarm) {
+        viewModelScope.launch {
+            alarmRepository.updateAlarm(alarm).onSuccess {
+                getAlarms()
+            }
+        }
+    }
+
+    fun selectAlarm(alarm: Alarm) {
+        viewModelScope.launch{
+            alarmRepository.getAlarm(alarm.id).onSuccess { alarm ->
+                _state.value = state.value.copy(selectedAlarm = alarm)
+            }
+        }
+    }
+
+    fun clearSelectedAlarm() {
+        _state.value = state.value.copy(selectedAlarm = null)
+    }
+
+
+
 
 }
